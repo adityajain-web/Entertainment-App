b54e61d617b4051a2e97f4ec69d5631bde6e207e
adityajain-web <you@example.com>
2020-12-10 20:27:28 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Tachyons from 'tachyons'
+import Movie from './component/Movie'
 
-function App() {
+const FEATURED_API = "https://api.themoviedb.org/3/movie/popular?api_key=1f4e8dffb1980fd4e4722530dd1334fd&page=1";
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=1f4e8dffb1980fd4e4722530dd1334fd&query="
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    getMovie(FEATURED_API);
+  }, [])
+
+  const getMovie = (API) => {
+    fetch(API).then(res => res.json()).then(data => {
+      setMovies(data.results);
+    });
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm) {
+      getMovie(SEARCH_API + searchTerm)
+      setSearchTerm('');
+    }
+  }
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input type="search" placeholder="search..." className="search br-pill" value={searchTerm} onChange={handleOnChange} />
+        </form>
       </header>
-    </div>
+      <div className="movie-container">
+        {movies.length > 0 && movies.map((movie) =>
+          <Movie key={movie.id} {...movie} />
+        )}
+      </div>
+    </>
   );
 }
 
